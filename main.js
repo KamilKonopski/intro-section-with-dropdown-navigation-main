3b8f5a9a35ddfc7226b9d8af409398759e34816b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-02 22:17:03 +0200

    add mobile styles and fuctions

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ const featuresNav = document.querySelector('.features-nav');
 const companyNav = document.querySelector('.company-nav');
 const featuresArrow = document.querySelector('#features img');
 const companyArrow = document.querySelector('#company img');
+const nav = document.querySelector('.nav');
+
+const mobileMenu = document.querySelector('.mobile-nav');
+const mobileMenuImg = document.querySelector('.mobile-nav img')
+
+mobileMenu.addEventListener('click', () => {
+    nav.classList.toggle('menu');
+
+    if(nav.classList.contains('menu')) {
+        mobileMenuImg.src = "./images/icon-close-menu.svg";
+        nav.style.zIndex = '2';
+        nav.style.opacity = '1';
+        mobileMenu.style.zIndex = '3';
+        nav.style.transform = 'translate(0)';
+    }else {
+        mobileMenuImg.src = "./images/icon-menu.svg";
+        nav.style.zIndex = '0';
+        nav.style.opacity = "0";
+        nav.style.transform = 'translate(150%)';
+    };
+});
 
 features.addEventListener('click', () => {
     featuresNav.classList.toggle('visible');
